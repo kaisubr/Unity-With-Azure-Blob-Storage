ba6b4e9a6370649d46718f6759f8f8c876c8ed7e
Kailash Sub <kailash.s.2012@gmail.com>
2019-11-05 01:07:11 -0600

    Init commit

---
+++ Assets/Scenes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+public class Program : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Script runs when cube is rendered.");
+        GameObject ifieldobj = GameObject.Find("Canvas/param");
+        InputField ifield = ifieldobj.GetComponent<InputField>();
+        ifield.onEndEdit.AddListener(ParameterInput);
+
+    }
+
+    //User enters connection string and hits Return key.
+    public void ParameterInput(string cxnstr)
+    {
+        Debug.Log(cxnstr);
+
+        CloudStorageAccount act = CloudStorageAccount.Parse(cxnstr);
+        CloudBlobClient client = act.CreateCloudBlobClient();
+
+        var container = client.GetContainerReference("example");
+        container.CreateIfNotExistsAsync().Wait();
+
+        CloudBlockBlob blob = container.GetBlockBlobReference("log.txt");
+        //blob.UploadTextAsync("Unity upload").Wait();
+        appendText(blob, "Unity log: " + System.DateTime.UtcNow.ToString("MM-dd-yyyy hh:mm:ss"));
+    }
+
+    public static async Task appendText(CloudBlockBlob blob, string v)
+    {
+        var upload = v;
+
+        if (await blob.ExistsAsync())
+        {
+            //append. here we test retrieval & read...
+            var content = await blob.DownloadTextAsync();
+
+            upload = content + "\n" + v;
+        }
+
+        blob.UploadTextAsync(upload).Wait();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity3D With Azure Blob Storage
+ This is a Unity demonstration that utilizes Azure Blob Storage to read/write to a log file. Client example usage for AR Sphere.
